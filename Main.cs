f9eb50daee8e7d782f7bc02cb4ace9bd78a6131e
Felipe Mourão <85915186+felipricardo@users.noreply.github.com>
2023-08-23 10:17:44 -0300

    Update Main.cs
    while

--- Main.cs
+++ Main.cs
@@ -6,33 +6,31 @@ namespace TeoremaDeTales
     {
         static void Main(string[] args)
         {
-            // Solicita as medidas dos segmentos do primeiro triangulo
-            Console.WriteLine("Digite as medidas dos segmentos do primeiro triangulo:");
+            // Solicita as medidas dos segmentos do primeiro triângulo
+            Console.WriteLine("Digite as medidas dos segmentos do primeiro triângulo:");
             double a1, b1, a2, b2;
 
-            // Obtém as medidas dos segmentos do primeiro triangulo usando a função TryGetValidInput
-            if (TryGetValidInput(out a1) && TryGetValidInput(out b1) &&
-                TryGetValidInput(out a2) && TryGetValidInput(out b2))
+            // Obtém as medidas dos segmentos do primeiro triângulo usando a função TryGetValidInput
+            while (!TryGetValidInput(out a1) || !TryGetValidInput(out b1) ||
+                   !TryGetValidInput(out a2) || !TryGetValidInput(out b2))
             {
-                // Verifica se os denominadores não são zero antes de calcular as proporções
-                if (b1 != 0 && b2 != 0)
-                {
-                    // Calcula as proporções
-                    double proporcaoA = a1 / a2;
-                    double proporcaoB = b1 / b2;
+                Console.WriteLine("Entrada inválida. Certifique-se de inserir números válidos.");
+            }
 
-                    // Exibe as proporções formatadas com duas casas decimais
-                    Console.WriteLine($"Proporcao entre os lados A: {proporcaoA:F2}");
-                    Console.WriteLine($"Proporcao entre os lados B: {proporcaoB:F2}");
-                }
-                else
-                {
-                    Console.WriteLine("Nao é possível calcular a proporção quando um dos lados e zero.");
-                }
+            // Verifica se os denominadores não são zero antes de calcular as proporções
+            if (b1 != 0 && b2 != 0)
+            {
+                // Calcula as proporções
+                double proporcaoA = a1 / a2;
+                double proporcaoB = b1 / b2;
+
+                // Exibe as proporções formatadas com duas casas decimais
+                Console.WriteLine($"Proporção entre os lados A: {proporcaoA:F2}");
+                Console.WriteLine($"Proporção entre os lados B: {proporcaoB:F2}");
             }
             else
             {
-                Console.WriteLine("Entrada invalida. Certifique-se de inserir numeros validos.");
+                Console.WriteLine("Não é possível calcular a proporção quando um dos lados é zero.");
             }
         }
 
